2ea3f63ed376d037c29788c2a22bde504ce4b476
Alexandr Shubskij <alexandrshubskij@gmail.com>
2019-12-15 19:05:45 +0300

    initial commit

---
+++ Kursach/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kursach
+//
+//  Created by Александр Шубский on 15.12.2019.
+//  Copyright © 2019 Александр Шубский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
